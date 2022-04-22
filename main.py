c05cf8a8e495979b58f7700c2ddc5b0bf2f47668
i.rakovich <ilyarak2000@gmail.com>
2022-04-22 20:30:23 +0300

    Добавил len

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print ("my name is Ilya")
+#print('Припев песни Eiffel 65 - Blue:')
+#print("I'm Blue")
+#print ('Da ba dee da ba dye\n' * 0)
+
+#surname = 'Rakovich'
+#name = 'Ilya'
+#fio = f'{surname [0]},{name[0]}'
+#print(fio)
+
+#string = "Bon Jovi - Livin On A Prayer"
+#print(string[:])
+#print(string[-6::2])
+
+#nickname = 'Bloha_V_SaRaffane'
+#if len(nickname) > 15:
+        print('Sorry, this nickname is too long')
