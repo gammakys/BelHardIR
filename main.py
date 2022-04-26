2bee451f0576d2fbeb979e7f8e9d3315ec9388b3
i.rakovich <ilyarak2000@gmail.com>
2022-04-26 15:46:20 +0300

    Закончил с ДЗ

--- main.py
+++ main.py
@@ -4,4 +4,31 @@ print('К неизменяемым относятся целые числа (int
       ' К изменяемым — списки (list), множества (set),'
       ' байтовые массивы (byte arrays) и словари (dict).')
 bool_var = True
+int_var = 1212
+float_var = 2110200012032001.00
+complex_var = 1+2+3j
+
+print(type(1))
+print(isinstance('Три медведя варят кашу', str))
+
+var = None
+num = 2288
+
+x = ('Price')
+y = (' ')
+z = ('20 bucks')
+print(x + y + z)
+
+print(bool(1))
+
+print("when would it end\n" * 3)
+life =('beautiful')
+ilness = f'{life[4:7]}'
+print(ilness)
+
+Food = ('Olives,snacks,chips')
+print(Food.split(','))
+
+
+
 
