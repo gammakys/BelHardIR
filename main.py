5ba33abc99982f5546d277cbc84c1ff8fce5de23
i.rakovich <ilyarak2000@gmail.com>
2022-04-28 15:01:22 +0300

    21.06

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print ("my name is Ilya")
+cont = 'Старт'
+
+while cont == 'Старт':
+    f_num =  float(input('Введите первое число'))
+    oper = input("Введите операцию")
+    s_num = float(input('Введите второе число'))
+    if oper == '+':
+        print(number_to_words(f_num + s_num))
+    elif oper == '-':
+        print(number_to_words(f_num - s_num))
+    elif oper == '*':
+        print(number_to_words(f_num * s_num))
+    elif oper == '/':
+        print(number_to_words(number_to_words(f_num / s_num)))
+    else:
+        print('Ошибка')
+    cont = input('Введите "Старт" если хотите попробовать еще раз; иное чтобы завершить')
+
+
+
+
+
+
