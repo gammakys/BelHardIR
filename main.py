1e30fdc8b9239f7acfa75f7040d373c772e37430
i.rakovich <ilyarak2000@gmail.com>
2022-04-28 15:19:03 +0300

    Готовый калькулятор

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ while cont == 'Старт':
     oper = input("Введите операцию")
     s_num = float(input('Введите второе число'))
     if oper == '+':
-        print(number_to_words(f_num + s_num))
+        print((f_num + s_num))
     elif oper == '-':
-        print(number_to_words(f_num - s_num))
+        print((f_num - s_num))
     elif oper == '*':
-        print(number_to_words(f_num * s_num))
+        print((f_num * s_num))
     elif oper == '/':
-        print(number_to_words(number_to_words(f_num / s_num)))
+        print(f_num / s_num)
     else:
         print('Ошибка')
     cont = input('Введите "Старт" если хотите попробовать еще раз; иное чтобы завершить')
