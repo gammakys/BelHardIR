3ba35f9cc9a41119a1cf562fb1855e5e9ebd9c3f
i.rakovich <ilyarak2000@gmail.com>
2022-05-16 23:29:27 +0300

    commit

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+r = float(input())
+p = 3.14
 
 
+class Shape:
+
+    def __init__(self, get_perimeter, get_square):
+        self.get_perimeter = perimeter
+        self.get_square = square
+
+    @abstractmethod
+    def get_perimeter(self, r):
+        print(2*p*r)
+
+        pass
+
+    @abstractmethod
+    def get_square(self, square):
+        print(p*r**2)
+        return
 
 
+def need_r(r):
+    r = input()
 
 
+class Circle(Shape):
 
-
-
+    def find_perimeter(get_perimeter):
+        return
 
