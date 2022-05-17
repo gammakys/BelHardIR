a17a4e24434ce3befe098aa162486c4114e14131
i.rakovich <ilyarak2000@gmail.com>
2022-05-17 08:57:51 +0300

    commit

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
-r = float(input())
-p = 3.14
-
-
-class Shape:
-
-    def __init__(self, get_perimeter, get_square):
-        self.get_perimeter = perimeter
-        self.get_square = square
-
-    @abstractmethod
-    def get_perimeter(self, r):
-        print(2*p*r)
-
-        pass
-
-    @abstractmethod
-    def get_square(self, square):
-        print(p*r**2)
-        return
-
-
-def need_r(r):
-    r = input()
-
-
-class Circle(Shape):
-
-    def find_perimeter(get_perimeter):
-        return
-
-class Rectangle:
-    a = float
-    b = float
-
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
+# r = float(input())
+# p = 3.14
 #
+#
+# class Shape:
+#
+#     def __init__(self, get_perimeter, get_square):
+#         self.get_perimeter = perimeter
+#         self.get_square = square
+#
+#     @abstractmethod
+#     def get_perimeter(self, r):
+#         print(2 * p * r)
+#
+#         pass
+#
+#     @abstractmethod
+#     def get_square(self, square):
+#         print(p * r ** 2)
+#         return
+#
+#     def need_r(r):
+#         input()
+#
+#
+# class Circle(Shape):
+#
+#     def find_perimeter(self):
+#         return
+#
+#
+# class Rectangle(Shape):
+#     a = float
+#     b = float
+#
+#     def __init__(self, a, b):
+#         super().__init__(self, a)
+#         super().__init__(self, b)
+#
+# class Square(Rectangle):
+#
+#     def __init__(self, f):
+#         input()
+#         super().__init__(self, f)
+#         self.f = f
+#
+# ##################################
+
+
