6445e8b17a7b6652945a64fe52539c528828327f
i.rakovich <ilyarak2000@gmail.com>
2022-05-22 13:42:14 +0300

    commit

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print ("my name is Ilya")
+# class Car:
+#     global speed
+#     brand: str
+#     model: str
+#     year_of_issue: str
+#     speed = 0
+#     count = 0
+#
+#     def __init__(self, brand, model, year_of_issue, speed):
+#         self.brand = brand
+#         self.model = model
+#         self.year_of_issue = year_of_issue
+#         self.speed = speed
+#
+#     def increase(self):
+#         speed += 5
+#         return speed
+#
+#     def decrease(self):
+#         speed -= 5
+#         return speed
+#
+#     def stop(self):
+#         speed = 0
+#         return speed
+#
+#     def status(self):
+#         return f"Car: {self.speed}"
+#
+#     def reversal(self):
+#         speed = self.speed * -1
+#
+#
+# car_set = Car('Tesla', 'X', 2012, 0)
+# print(car_set.decrease())
+
+#######################
